91c3f92a313d9712a33f7f84b3ce7036985c1958
Marcus Pereira <marcus.work@hotmail.com>
2020-09-20 10:01:11 -0300

    Calculating subtotal of the prices and total of items

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -47,15 +47,20 @@ const Cart: React.FC = () => {
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((acc, item) => {
+      const calc = item.price * item.quantity;
+      return acc + calc;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((acc, item) => {
+      return acc + item.quantity;
+    }, 0);
 
-    return 0;
+    return total;
   }, [products]);
 
   return (
